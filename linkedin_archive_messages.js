e2ae8dbc84dc0cc33d9a73e0d1d104be47f4330f
Gaidar Magdanuov <gaidar@users.noreply.github.com>
2023-11-10 15:39:13 +0100

    Update linkedin_archive_messages.js

--- linkedin_archive_messages.js
+++ linkedin_archive_messages.js
@@ -2,9 +2,9 @@ timer = setInterval(() => {
     
     // works as of Nov 9, 2023
     // select all messages
-    items = document.querySelectorAll('.msg-selectable-entity__entity');
+    items = document.querySelectorAll('.msg-selectable-entity__checkbox-circle-container');
     for (let i = 0; i < items.length; i++) {
-        items[i].click
+        items[i].click();
     }
     // click Archive button
     button = document.getElementById('ember356');
